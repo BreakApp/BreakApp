be856a1d51f451d7f4480abb7cf84f8d740b3687
jwarielle <jwarielle@gmail.com>
2014-09-22 14:35:34 -0700

    Setting up Db and the server, model, route, and service for DB.

--- app/js/services/break-service.js
+++ app/js/services/break-service.js
@@ -1,13 +1,19 @@
 // breakService
 
 module.exports = function(app) {
-  app.factory('breakService', function() {
+  app.factory('breakService', function($http) {
     // helper functions...
 
     // breakScrambler object passed to controller
     var breakScrambler = {
       getBreak: function() {
-        return "Here's a break!";
+        var dbBreak = $http({
+          method: 'GET',
+          url: '/api/v_0_0_1/breakideas'
+        }).error(function(data, status) {
+          console.log(status);
+        });
+        return dbBreak;
       }
     };
     return breakScrambler;
--- server.js
+++ server.js
@@ -2,19 +2,23 @@
 
 var express = require('express');
 var bodyparser = require('body-parser');
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 var http = require('http');
+
 var app = express();
 
-// mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/notes-development');
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/breaks');
 
 app.use(express.static(__dirname + (process.env.STATIC_DIR || '/build')));
 
 app.use(bodyparser.json());
-// require('./routes/note-routes')(app);
+require('./routes/studyBreaks-routes')(app);
+//If we need body parser, we will say: app.use(bodyparser.json());
+// Our route to the breaks db, it will be different that this route: require('./routes/note-routes')(app);
+// Our route to the users db
 
 var server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000, function() {
-  console.log('server running on port 3000');
+  console.log('server running');
 });
