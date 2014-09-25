6c4a3c8c95b923112ecbadac19bae57e1acc0441
Jordan Buckley <jordan@bkly.org>
2014-09-25 11:14:05 -0700

    footer directive & modals working

--- app/js/controllers/break-controller.js
+++ app/js/controllers/break-controller.js
@@ -5,17 +5,18 @@ module.exports = function(app) {
   app.controller('breakController', function($scope, breakService) {
 
     $scope.currentBreak = '';
-
-    $scope.timeoutID;
     $scope.timerLength = 2000;
+    $scope.timerRunning = false;
 
 
     $scope.breakTimer = function(){
       $scope.currentBreak = '';
       $scope.timeoutID = setTimeout($scope.getBreak, $scope.timerLength);
+      $scope.timerRunning = true;
     };
 
     $scope.getBreak = function() {
+      $scope.timerRunning = false;
       breakService.getBreak().success(function(data) {
         var randomSeed = Math.floor((Math.random() * data.length));
  	    	$scope.currentBreak = data[randomSeed];
